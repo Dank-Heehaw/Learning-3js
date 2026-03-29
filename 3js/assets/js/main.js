ca3d9fd20be58717f082b2280ff181fab24de35a
Abdul Waheed Khan Sowdagar <39850943+Dank-Heehaw@users.noreply.github.com>
2026-03-29 01:10:14 -0400

    change dir

--- 3js/assets/js/main.js
+++ 3js/assets/js/main.js
@@ -1,4 +1,4 @@
-import "./scss/style.scss";
+import "../../../src/scss/style.scss";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Learning-3js/"
+  base: "/Learning-3js/",
+  build: {
+    outDir: "3js",
+    assetsDir: "assets",
+    emptyOutDir: false
+  }
 });
